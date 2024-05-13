5b28eb2229654397923d64fb78e7cafcf47e09c3
jmgv87191 <jmgv87191@gmail.com>
2024-05-12 18:43:14 -0700

    respaldo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: HomeComponent
+    },
+    {
+        path:'portafolio',
+        component:PortafolioComponent 
+    },
+    {
+        path:'about',
+        component:PortafolioComponent 
+    },
+    {
+        path:'**', pathMatch:'full',redirectTo:''
+    },
+
+];
--- src/app/components/custom-sidenav/custom-sidenav.component.ts
+++ src/app/components/custom-sidenav/custom-sidenav.component.ts
@@ -1,30 +1,43 @@
-import { Component, signal } from '@angular/core';
+import { Component, Input, computed, signal } from '@angular/core';
 import { MenuItem } from '../../interfaces/menu-item';
 import { MatListModule } from '@angular/material/list'
 import { MatIconModule } from '@angular/material/icon'
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-custom-sidenav',
   standalone: true,
   imports: [MatListModule,
-    MatIconModule
+    MatIconModule,
+    CommonModule,
+    RouterLink,
+    RouterModule
   ],
   templateUrl: './custom-sidenav.component.html',
   styleUrl: './custom-sidenav.component.css'
 })
 export class CustomSidenavComponent {
 
+  sideNavCollapsed = signal(false);
+  @Input() set collapsed( val: boolean ){
+    this.sideNavCollapsed.set(val)
+  }
+
   menuItems = signal<MenuItem[]>([
     {
       icon: 'analytics',
-      label: 'Analytics',
-      route: 'analytics'
+      label: 'Home',
+      route: ''
     },
     {
       icon: 'comments',
-      label: 'Comments',
-      route: 'comments'
+      label: 'Sobre mi',
+      route: '/about'
     },
   ]);
+
+  profilePicSize = computed(()=>this.sideNavCollapsed()? '32': '100' )
 
 }
